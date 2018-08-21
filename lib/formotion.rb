ce21b0b862af9f37017c9a6caa062d0ba0b7f685
Fabian Schwahn <fabian.schwahn@gmail.com>
2018-08-21 10:37:00 +0200

    Sort dependencies to fix problems with AFS

--- lib/formotion.rb
+++ lib/formotion.rb
@@ -5,11 +5,11 @@ require 'bubble-wrap/camera'
 
 require 'motion-require'
 
-Motion::Require.all(Dir.glob(File.expand_path('../formotion/**/*.rb', __FILE__)))
+Motion::Require.all(Dir.glob(File.expand_path('../formotion/**/*.rb', __FILE__)).sort)
 
 Motion::Project::App.setup do |app|
   app.frameworks<<'CoreLocation' unless app.frameworks.include?('CoreLocation')
   app.frameworks<<'MapKit' unless app.frameworks.include?('MapKit')
-  
+
   app.resources_dirs << File.join(File.dirname(__FILE__), '../resources')
 end
