c01acb36f12068fddf92fe317538c5e910f4d52a
Fabian Schwahn <fabian.schwahn@gmail.com>
2025-02-14 15:48:40 +0100

    Disable placeholder functionality
    When compiling with iOS 18.1, we started seeing crashes from the `dealloc`-method of this placeholder module.
    
    To prevent those, we simply disable this code path completely, as we don't require it.

--- lib/formotion/patch/ui_text_view_placeholder.rb
+++ lib/formotion/patch/ui_text_view_placeholder.rb
@@ -1,75 +1,78 @@
 class UITextView
-  attr_reader :placeholder
-  attr_accessor :placeholder_color
+  # Disable all functionality for placeholders
+  attr_accessor :placeholder, :placeholder_color
 
-  alias_method :old_initWithCoder, :initWithCoder
-  def initWithCoder(decoder)
-    old_initWithCoder(decoder)
-    setup
-    self
-  end
+  # attr_reader :placeholder
+  # attr_accessor :placeholder_color
 
-  alias_method :old_initWithFrame, :initWithFrame
-  def initWithFrame(frame)
-    old_initWithFrame(frame)
-    setup
-    self
-  end
+  # alias_method :old_initWithCoder, :initWithCoder
+  # def initWithCoder(decoder)
+  #   old_initWithCoder(decoder)
+  #   setup
+  #   self
+  # end
 
-  def setup
-    @foreground_observer = NSNotificationCenter.defaultCenter.observe UITextViewTextDidChangeNotification do |notification|
-      updateShouldDrawPlaceholder
-    end
-  end
+  # alias_method :old_initWithFrame, :initWithFrame
+  # def initWithFrame(frame)
+  #   old_initWithFrame(frame)
+  #   setup
+  #   self
+  # end
 
-  alias_method :old_dealloc, :dealloc
-  def dealloc
-    NSNotificationCenter.defaultCenter.unobserve @foreground_observer
-    old_dealloc
-  end
+  # def setup
+  #   @foreground_observer = NSNotificationCenter.defaultCenter.observe UITextViewTextDidChangeNotification do |notification|
+  #     updateShouldDrawPlaceholder
+  #   end
+  # end
 
-  alias_method :old_drawRect, :drawRect
-  def drawRect(rect)
-    old_drawRect(rect)
+  # alias_method :old_dealloc, :dealloc
+  # def dealloc
+  #   NSNotificationCenter.defaultCenter.unobserve @foreground_observer
+  #   old_dealloc
+  # end
 
-    if (@shouldDrawPlaceholder)
-        self.placeholder_color.set
-        self.font ||= UIFont.systemFontOfSize(17) # ios7 seems to have a bug where font of uitextview isn't set by default
-        self.placeholder.drawInRect(placeholder_rect, withFont:self.font)
-    end
-  end
+  # alias_method :old_drawRect, :drawRect
+  # def drawRect(rect)
+  #   old_drawRect(rect)
+
+  #   if (@shouldDrawPlaceholder)
+  #       self.placeholder_color.set
+  #       self.font ||= UIFont.systemFontOfSize(17) # ios7 seems to have a bug where font of uitextview isn't set by default
+  #       self.placeholder.drawInRect(placeholder_rect, withFont:self.font)
+  #   end
+  # end
 
 
-  alias_method :old_setText, :setText
-  def setText(text)
-    old_setText(text)
+  # alias_method :old_setText, :setText
+  # def setText(text)
+  #   old_setText(text)
 
-    updateShouldDrawPlaceholder
-  end
+  #   updateShouldDrawPlaceholder
+  # end
 
-  def placeholder=(placeholder)
-    return if @placeholder == placeholder
+  # def placeholder=(placeholder)
+  #   return if @placeholder == placeholder
 
-    @placeholder = placeholder
+  #   @placeholder = placeholder
 
-    updateShouldDrawPlaceholder
-  end
+  #   updateShouldDrawPlaceholder
+  # end
 
-  def placeholder_rect
-    x_offset = font.xHeight
-    y_offset = font.capHeight + font.descender
+  # def placeholder_rect
+  #   x_offset = font.xHeight
+  #   y_offset = font.capHeight + font.descender
 
-    CGRectMake(self.contentInset.left + x_offset, self.contentInset.top + y_offset, self.frame.size.width - self.contentInset.left - self.contentInset.right - x_offset, self.frame.size.height - self.contentInset.top - self.contentInset.bottom - y_offset)
-  end
+  #   CGRectMake(self.contentInset.left + x_offset, self.contentInset.top + y_offset, self.frame.size.width - self.contentInset.left - self.contentInset.right - x_offset, self.frame.size.height - self.contentInset.top - self.contentInset.bottom - y_offset)
+  # end
 
-  def placeholder_color
-    @placeholder_color ||= UIColor.lightGrayColor
-  end
+  # def placeholder_color
+  #   @placeholder_color ||= UIColor.lightGrayColor
+  # end
 
-  def updateShouldDrawPlaceholder
-    prev = @shouldDrawPlaceholder;
-    @shouldDrawPlaceholder = self.placeholder && self.text.length == 0
+  # def updateShouldDrawPlaceholder
+  #   prev = @shouldDrawPlaceholder;
+  #   @shouldDrawPlaceholder = self.placeholder && self.text.length == 0
 
-    self.setNeedsDisplay if (prev != @shouldDrawPlaceholder)
-  end
+  #   self.setNeedsDisplay if (prev != @shouldDrawPlaceholder)
+  # end
 end
